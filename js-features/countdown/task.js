4cba478a54d72e15f7155698028c597a4ecdd0b1
Irina 13 <LIA130594@yandex.ru>
2024-10-30 15:15:34 +0500

    first commit countdown

---
+++ js-features/countdown/task.js
@@ -0,0 +1,12 @@
+const countDown = function() {
+    const timer = document.getElementById("timer");
+    timer.textContent --;
+    if (timer.textContent <= 0) {
+        clearInterval(id);
+        alert("Вы победили в конкурсе!");
+    }
+};
+
+const id = setInterval(countDown, 500);
+
+
